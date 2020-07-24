995bb34c097014c8ecb5ff9966ece9fcf4101a48
Amir <hesamian@uwm.edu>
2020-07-23 23:13:11 -0500

    Made the code aync

--- app.py
+++ app.py
@@ -1,47 +1,54 @@
-import requests
 from bs4 import BeautifulSoup
-import re
-import urllib
+import re, urllib, requests, sys
+import asyncio
 
+# List of visited sites
+visited_sites = []
 
 def is_url(url):
     regex = re.compile(
-        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'^(?:http|ftp)s?://'                                                                 # http:// or https://
         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-        r'localhost|'  # localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-        r'(?::\d+)?'  # optional port
+        r'localhost|'                                                                         # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'                                                # ...or ip
+        r'(?::\d+)?'                                                                          # optional port
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
     return re.match(regex, url) is not None
 
 
-visited_sites = []
+async def crawler(url, depth, max_depth):
+  # If max depth reached
+  if depth >= max_depth:
+    return
+
+  try:
+      # If url is not valid
+      if not is_url(url):
+          return
+
+      # Resolve the base url
+      base_url = urllib.parse.urlparse(url).hostname
+
+      # If base url is already visited
+      if base_url in visited_sites:
+          return
+
+      visited_sites.append(base_url)
+
+      print(f"{depth} => {base_url}")
+      r = requests.get(url)
+      text = r.text
+      html = BeautifulSoup(text, 'html.parser')
+      anchor_tags = html.find_all('a')
+      urls = [a.get('href') for a in anchor_tags]
+
+      await asyncio.wait([crawler(nested_url, depth + 1, max_depth) for nested_url in urls])
+  except:
+      return
 
 
-def crawler(url, depth):
-    try:
-        if not is_url(url):
-            return
+url = sys.argv[1]                       # url
+max_depth = int(sys.argv[2])            # max depth
 
-        base_url = urllib.parse.urlparse(url).hostname
-
-        if base_url in visited_sites:
-            return
-
-        visited_sites.append(base_url)
-
-        print(f"{depth} => {base_url}")
-        r = requests.get(url)
-        text = r.text
-        html = BeautifulSoup(text, 'html.parser')
-        anchor_tags = html.find_all('a')
-        urls = [a.get('href') for a in anchor_tags]
-
-        for nested_url in urls:
-            crawler(nested_url, depth + 1)
-    except:
-        return
-
-
-crawler("https://realpython.com/python-requests/", 1)
+asyncio.run(crawler(url, 1, max_depth)) # start crawler
